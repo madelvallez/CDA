2e40eca865a306d980f981ec48f1f3b24740556d
marine <marine.delvallez@etu.univ-orleans.fr>
2023-03-22 19:25:19 +0100

    22/03 10:35 Marine

--- modele/Noeud.java
+++ modele/Noeud.java
@@ -1,12 +1,11 @@
 package modele;
 
 import java.util.SortedSet;
-import java.util.List;
-import java.util.SortedMap;
+import java.util.ArrayList;
 
 import static modele.Plateau.NOIR;
 import static  modele.Plateau.BLANC;
-public class Noeud {
+public class Noeud implements Comparable<Noeud>{
     private Coup coup;
     private Plateau plateau;
     private boolean isMax;
@@ -16,53 +15,68 @@ public class Noeud {
 
     private SortedSet<Noeud> filsPossibles;
 
-    public Noeud(Coup c, Plateau p, boolean isMax, String coul){
+    public Noeud(Coup c, Plateau plateau_parent, boolean isMax, String coul){
         this.coup = c;
-        this.plateau = p.copie();
+        this.plateau = plateau_parent.copie();
+        this.plateau.appliqueCoup(c);
         this.isMax = isMax;
         this.couleur = coul;
     }
 
-    private void calculerScore(int profCalc, List<Coup> coupsPossibles){
+
+
+    private void calculerScore(int profCalc){
         if (!scoreConnu){
-            score = this.minMax(profCalc, isMax, coupsPossibles).score;
+            score = this.minMax(profCalc).score;
         }
     }
 
-    private Noeud minMax(int profCalc, boolean isMax, List<Coup> coupsPossibles){
-        if (coupsPossibles.size()==0){
-           //cherher qui a gangé
-            int nbPionJoueur = this.plateau.score(this.couleur);
-            int nbPionAdv = this.plateau.score(Plateau.opposeCouleur(this.couleur));
-            if (nbPionAdv>nbPionJoueur){
-                this.score = -1000;
-            }else{
-                this.score = 1000;
+    private Noeud minMax(int profCalc){
+        ArrayList<Coup> coupsPossibles = this.plateau.listeCoupsPossibles(this.couleur);
+        if (coupsPossibles.size()==0){ //si pas de coup possible
+            if (this.plateau.estFiniePartie()){ //si c'est une configuration finale
+                //cherher qui a gagné
+                int nbPionJoueur = this.plateau.score(this.couleur);
+                int nbPionAdv = this.plateau.score(Plateau.opposeCouleur(this.couleur));
+                if (nbPionAdv>nbPionJoueur){
+                    this.score = -1000;
+                }else{
+                    this.score = 1000;
+                }
+                return this;
+            }else{ // sinon, coupPasser est le seul coup possible
+                Coup cFils = Coup.coupPasser();
+                filsPossibles.add(new Noeud(cFils, this.plateau, !isMax, Plateau.opposeCouleur(couleur)));
+                calculerScore(profCalc-1);
+                assert filsPossibles.size()==1;
+                return filsPossibles.first();
             }
-        } else {
-            for (Coup c : coupsPossibles) {
-                this.filsPossibles.add(new Noeud(c, plateau, !isMax, Plateau.opposeCouleur(this.couleur)));
-                //appliquer le coup sur le plateau
-            }
-            if (profCalc == 0) {//pas besin de calculer les scores
+        } else { //il y a des coups possibles : evaluons-les puis choisisons le bon
+            if (profCalc == 0) {//pas besoin de calculer les fils
                 this.score = this.fonctionEvaluation();
                 return this;
-            } else {
-                for (Noeud f : filsPossibles) {
-                    List<Coup> coupsFils = f.plateau.listeCoupsPossibles(f.couleur);
-                    calculerScore(profCalc - 1, coupsFils);
+            } else { //caculons les fils pour choisir le bon
+                for (Coup c : coupsPossibles) {//création des fils du noeud courant
+                this.filsPossibles.add(new Noeud(c, this.plateau, !isMax, Plateau.opposeCouleur(this.couleur)));
                 }
+                for (Noeud f : filsPossibles) { //calcul des scores des fils (appe rec caché)
+                    calculerScore(profCalc - 1);
+                }
+                //recherhe du meilleur coup
+                Noeud meilleur ;
                 if (isMax) {
-                    Noeud max = ;
-                    for (Noeud n : filsPossibles) {
-                        if (max.score < n.score) {
-
-                        }
-                    }
+                    meilleur = filsPossibles.first();
+                }else{
+                    meilleur = filsPossibles.last();
                 }
+                return meilleur;
             }
         }
     }
 
 
+    @Override
+    public int compareTo(Noeud noeud) {
+        return this.score - noeud.score ;
+    }
 }
