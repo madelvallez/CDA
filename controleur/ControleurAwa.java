b5421143f45122d5e10d3108c413c3f4d718c3f6
ThibautMartiin <121037813+ThibautMartiin@users.noreply.github.com>
2023-04-15 15:10:18 +0200

    debutModifThib

---
+++ controleur/ControleurAwa.java
@@ -0,0 +1,14 @@
+package controleur;
+
+import vue.Ihm;
+
+public class ControleurAwa extends Controleur{
+
+
+    public ControleurAwa(Ihm ihm) {
+        super(ihm);
+    }
+
+    public void jouerAwa() {
+    }
+}
